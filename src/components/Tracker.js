ccef9d2dce390fcd186f18145b59b0b384536f39
ShanaySharif <Shanay.uw17@gmail.com>
2023-09-09 16:59:14 -0400

    work in progress

---
+++ src/components/Tracker.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NewTrackerForm from "./NewTicketForm";
+import TrackerList from "./TicketList";
+import TrackerDetail from "./TicketDetail";
+
+
+
+
+
+
+
+
+export default TrackerControl
