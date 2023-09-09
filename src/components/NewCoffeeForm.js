5efedbf696172153f71491110f1033bda433e1c3
ShanaySharif <Shanay.uw17@gmail.com>
2023-09-09 18:46:17 -0400

    add NewCoffeeForm

--- src/components/NewCoffeeForm.js
+++ src/components/NewCoffeeForm.js
@@ -3,7 +3,45 @@ import PropTypes from "prop-types"
 import { v4 } from 'uuid'; // new code
 
 
-
+function NewCoffeeForm(props){
+      function handleNewCoffeeFormSubmission(event) {
+        event.preventDefault();
+        props.onNewCoffeeCreation({
+          names: event.target.names.value, 
+          origin: event.target.origin.value, 
+          price: event.target.price.value, 
+          roast: event.target.roast.value, 
+          id: v4()
+        });
+      }
+    
+      NewCoffeeForm.propTypes = {
+        onNewCoffeeCreation: PropTypes.func
+      };
+    
+      return (
+        <React.Fragment>
+          <form onSubmit={handleNewCoffeeFormSubmission}>
+            <input
+              type='text'
+              name='names'
+              placeholder='Coffee Name' />
+            <input
+              type='text'
+              name='origin'
+              placeholder='Origin' />
+              <input
+              type='text'
+              name='price'
+              placeholder='price' />
+            <textarea
+              name='roast'
+              placeholder='Roast.' />
+            <button type='submit'>Submit!</button>
+          </form>
+        </React.Fragment>
+      );   
+    }
 
 
 
