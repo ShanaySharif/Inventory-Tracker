3424e5ed63f6f96238c5c9a22b7611704d788fd6
ShanaySharif <Shanay.uw17@gmail.com>
2023-09-09 18:27:38 -0400

    Add PropTypes to CoffeeList component

--- src/components/CoffeeList.js
+++ src/components/CoffeeList.js
@@ -2,6 +2,31 @@ import React from 'react';
 import Coffee from './Coffee';
 import PropTypes from "prop-types";
 
+function CoffeeList(props){ 
+    return (
+        <React.Fragment>
+          <hr/>
+          {props.coffeeList.map((coffee) => 
+            <Coffee
+            whenCoffeeClicked = { props.onCoffeeSelection }
+              names={coffee.names}
+              origin={coffee.origin}
+              price={coffee.price}
+              roast={coffee.roast}
+              id = {coffee.id}
+              key={coffee.id}/>
+          )}
+        </React.Fragment>
+    );
+  }
+
+  //Add propTypes for ticketList.
+  CoffeeList.propTypes = {
+    coffeeList: PropTypes.array,
+    onCoffeeSelection: PropTypes.func
+  };
+      
+  
 
 
 
