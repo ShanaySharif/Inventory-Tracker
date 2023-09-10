5ee37dc315ab6657a2f67ea6c7bd792b1b1eb33e
ShanaySharif <Shanay.uw17@gmail.com>
2023-09-10 09:24:11 -0400

    add eventlistener to edit form

--- src/components/EditCoffeeForm.js
+++ src/components/EditCoffeeForm.js
@@ -4,9 +4,19 @@ import PropTypes from "prop-types";
 
 
 function EditCoffeeForm (props) {
+  const { coffee } = props;
+
+  function handleEditCoffeeFormSubmission(event) {
+    event.preventDefault();
+    props.onEditCoffee({names: event.target.names.value, origin: event.target.origin.value, price: event.target.price.value,
+      roast: event.target.roast.value,
+      id: coffee.id});
+  }
+
   return (
     <React.Fragment>
       <ReusableForm 
+       formSubmissionHandler={handleEditCoffeeFormSubmission} 
         buttonText="Update Coffee" />
     </React.Fragment>
   );
